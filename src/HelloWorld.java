9b3cd1f47155ac815f209a94276d23caa2b9bd1f
KON YUKI <tsriders.k.y@icloud.com>
2016-02-12 16:53:20 +0900

    HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+
+/**
+ * @author internous
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+        System.out.println("HelloWorld");
+
+	}
+
+}
