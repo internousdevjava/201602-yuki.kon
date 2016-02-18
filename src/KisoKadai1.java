b5d1222de691dc358c3c9687c192273c5b6b1c41
KON YUKI <tsriders.k.y@icloud.com>
2016-02-18 11:59:44 +0900

    訂正いたしました

--- src/KisoKadai1.java
+++ src/KisoKadai1.java
@@ -10,30 +10,26 @@ public class KisoKadai1 {
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			System.out.println("九九表 \n縦列の値を入力してね");
 
-
-
 			str = br.readLine();
 			x = Integer.parseInt(str);
 			System.out.print("横列の値を入力して下さい");
 			str = br.readLine();
 			y = Integer.parseInt(str);
 
-			for (int i = 1; i <= 9; i++) {
+			for (int i = 1; i <= 100; i++) {
 
-				for (int j = 1; j <= 9; j++) {
+				for (int j = 1; j <= 100; j++) {
 					if (i <= x) {
 						if (j <= y) {
 							System.out.print((i * j) + " ");
-
 						}
 					}
-
 				}
 				System.out.println("");
 				if (isNumber(str)) {
 					System.out.print("次の数値を入力して下さい");
 					break;
-				} else if (x == 0) {
+				} else if (x == 0 && y == 0) {
 					System.out.print("もう一度入力して下さい");
 					continue;
 				}
@@ -43,6 +39,7 @@ public class KisoKadai1 {
 			System.out.println("入力エラー\n 最初からやり直し");
 		}
 	}
+
 	private static boolean isNumber(String str) {
 		return false;
 	}
