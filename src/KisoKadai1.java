4d8adf1a4f7f319458eb34c149f43f4fd09d30d5
KON YUKI <tsriders.k.y@icloud.com>
2016-02-18 13:58:18 +0900

    kisokadai1修正いたしました

--- src/KisoKadai1.java
+++ src/KisoKadai1.java
@@ -1,46 +1,113 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ *
+ */
+
+/**
+ * @author internous
+ *
+ */
 public class KisoKadai1 {
-	public static void main(String[] args) {
-		int x = 0;
-		int y = 0;
-		String str = null;
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			System.out.println("九九表 \n縦列の値を入力してね");
 
-			str = br.readLine();
-			x = Integer.parseInt(str);
-			System.out.print("横列の値を入力して下さい");
-			str = br.readLine();
-			y = Integer.parseInt(str);
+    /**
+     * @param args
+     * @return
+     */
+    public static void main(String[] args) {
 
-			for (int i = 1; i <= 100; i++) {
+        System.out.println("九九表");
 
-				for (int j = 1; j <= 100; j++) {
-					if (i <= x) {
-						if (j <= y) {
-							System.out.print((i * j) + " ");
-						}
-					}
-				}
-				System.out.println("");
-				if (isNumber(str)) {
-					System.out.print("次の数値を入力して下さい");
-					break;
-				} else if (x == 0 && y == 0) {
-					System.out.print("もう一度入力して下さい");
-					continue;
-				}
-			}
+        boolean f = true;
 
-		} catch (Exception e) {
-			System.out.println("入力エラー\n 最初からやり直し");
-		}
-	}
+        String str = null;
+        int y;
+        while (f) {
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	private static boolean isNumber(String str) {
-		return false;
-	}
+                System.out.println("行数を入力してください。"
+                        + "(行数・列数ともに数字は1～9までです。)");
+                str = br.readLine();
+
+                if (isNumber(str)) {
+                    y = Integer.parseInt(str);
+                    if (y >= 1 && y < 100) {
+
+                        break;
+                    } else {
+                        System.out.println("1～99以外の数字になっていませんか？"
+                                + "\n再度1～9のまでの数字を入力して下さい。");
+                    }
+                } else {
+                    System.out.println("数字以外になっていないですか？"
+                            + "\n再度1～9までの数字を入力して下さい。");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        String str2 = null;
+        int x;
+        while (f) {
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("列数を入力してください。");
+                str2 = br.readLine();
+
+                if (isNumber(str2)) {
+                    x = Integer.parseInt(str2);
+                    if (x >=1 && x < 100) {
+                        break;
+                    } else {
+                        System.out.println("1～99以外の数字になっていませんか？"
+                                + "\n再度1～9までの数字を入力して下さい。");
+                    }
+                } else {
+                    System.out.println("数字以外になっていないですか？"
+                            + "\n再度1～9までの数字を入力して下さい。");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        y = Integer.parseInt(str);
+        x = Integer.parseInt(str2);
+
+        for (int i = 1; i <= y; i++) {
+            for (int j = 1; j <= x; j++) {
+                if (i * j < 10) {
+                    System.out.print("   " + (i * j));
+                    System.out.print(" ");
+                }
+                else if(i * j < 100){
+                    System.out.print("  " + (i * j));
+                    System.out.print(" ");
+                }
+                else if(i * j < 1000){
+                    System.out.print(" " + (i * j));
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print(i * j);
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+
+    }
+    public static boolean isNumber(String val) {
+        try {
+            Integer.parseInt(val);
+            return true;
+        } catch (NumberFormatException nfex) {
+            return false;
+        }
+
+    }
 }
